af452788970f8a67c4429383cdc16f3704368779
tgrochow <t_grochow@gmx.de>
2013-08-29 22:39:25 +0200

    update reflection

--- test.cpp
+++ test.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc,char * argv[])
 {
-   graphic::Sdf_parser parser("triangle.sdf");
+   graphic::Sdf_parser parser("reflection.sdf");
 	graphic::Scene scene(parser.parse_scene());
 	scene.save();
 	
